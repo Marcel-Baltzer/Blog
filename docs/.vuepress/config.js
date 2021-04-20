86f3492e6b6f8b48a99ef6195904a2439170e567
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-04-20 20:33:08 +0200

    BLOG-2 init theme structure

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,5 @@
 module.exports = {
-    plugins: [
-        [
-            '@vuepress/blog',
-        {
-            directories: [
-                {
-                    // Unique ID of current classification
-                    id: 'post',
-                    // Target directory
-                    dirname: '_posts',
-                    // Path of the `entry page` (or `list page`)
-                    path: '/',
-                },
-            ],
-        }
-    ],
-]
+    themeConfig: {
+        search: false,
+    }
 }
