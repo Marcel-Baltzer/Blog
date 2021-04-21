f6bf86fde01d1408f67e2a2c613b2e7e509bca47
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-04-21 13:09:09 +0200

    BLOG-2 add vuetify

---
+++ docs/.vuepress/theme/enhanceApp.js
@@ -0,0 +1,15 @@
+// register vuetify as a global plugin with vuepress
+// .vuepress/enhanceApp.js
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
+//import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+export default ({
+    Vue,      // the version of Vue being used in the VuePress app
+    options,  // the options for the root Vue instance
+    router,   // the router instance for the app
+    siteData  // site metadata
+}) => {
+    Vue.use(Vuetify)
+    options.vuetify = new Vuetify({})
+}
