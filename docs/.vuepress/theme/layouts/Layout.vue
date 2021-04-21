8f121be1e4b0ba217ca9b5cebc9e4f414cb59836
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-04-21 13:09:38 +0200

    BLOG-3 add darkmode button

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,4 +1,33 @@
 <template>
-    <div>
-    </div>
+    <v-app>
+        <v-main>
+            <v-container>
+                <v-row>
+                    <v-spacer></v-spacer>
+                    <ToggleTheme />
+                </v-row>
+
+                <label>"Hello Layout"</label>
+            </v-container>
+        </v-main>        
+
+        <v-footer padless>
+            <v-col
+            class="text-center"
+            cols="12"
+            >
+            {{ new Date().getFullYear() }} — <strong>Marcel Baltzer</strong>
+            </v-col>
+        </v-footer>
+    </v-app>
 </template>
+
+<script>
+    import ToggleTheme from '@theme/components/ToggleTheme.vue'
+
+    export default {
+        components: {            
+            ToggleTheme
+        }
+    }
+</script>
