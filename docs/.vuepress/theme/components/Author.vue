a12398f46facd4595eae8b552628bacee921636c
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-04-23 00:06:43 +0200

    BLOG-4 add contact Icons

--- docs/.vuepress/theme/components/Author.vue
+++ docs/.vuepress/theme/components/Author.vue
@@ -5,9 +5,38 @@
         </v-img>
         <h1 class="author__site-title">.Net Dev Blog</h1>
         <p class="author__intro">I am a passionate Software Engineer, I love the phoenix project ♥ ...</p>
+
+		<a :href="'https://github.com/Marcel-Baltzer'">
+            <GithubIcon />			
+        </a>
+
+		<a :href="'https://twitter.com/marcelbaltzer'">
+            <TwitterIcon />			
+        </a>
+
+		<a :href="'https://www.linkedin.com/in/marcel-baltzer-4a369a93/'">
+            <LinkedinIcon />			
+        </a>
 		
     </div>
 </template>
+
+<script>
+import {
+	GithubIcon,
+	TwitterIcon,
+	LinkedinIcon,
+} from 'vue-feather-icons'
+
+export default {
+	components: {
+    	GithubIcon,
+    	TwitterIcon,
+    	LinkedinIcon
+}
+}
+</script>
+
 
 <style lang="stylus">
 .author {
