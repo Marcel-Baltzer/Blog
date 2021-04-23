44046f07a7828391787bfa0a8b01f22cd695138c
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-04-23 22:01:17 +0200

    BLOG-4 cleanup author styles

--- docs/.vuepress/theme/components/Author.vue
+++ docs/.vuepress/theme/components/Author.vue
@@ -1,10 +1,15 @@
 <template>
-    <div max-width="500" align="center">                    
+    <div class="author">                    
         <v-img class="author__image" :src="$withBase('/avatar.png')">
         
         </v-img>
-        <h1 class="author__site-title">.Net Dev Blog</h1>
-        <p class="author__intro">I am a passionate Software Engineer, I love the phoenix project ♥ ...</p>
+        <h1 class="author__site-title">
+			.Net Dev Blog
+		</h1>
+        
+		<p class="author__intro">
+			I am a passionate Software Engineer, I love the phoenix project ♥ ...
+		</p>
 
 		<a :href="'https://github.com/Marcel-Baltzer'">
             <GithubIcon />			
@@ -30,37 +35,41 @@ import {
 
 export default {
 	components: {
-    	GithubIcon,
-    	TwitterIcon,
-    	LinkedinIcon
+		GithubIcon,
+		TwitterIcon,
+		LinkedinIcon
 }
 }
 </script>
 
 
 <style lang="stylus">
-.author {
-	&__image {
+.author
+	margin: 0 auto;
+	max-width: 500px;
+	text-align: center;
+	padding: calc(var(--space) / 2) 0;
+
+	&__image
 		border-radius: 100%;
-		width: 180px;
-		height: 180px;
+		width: 90px;
+		height: 90px;
 		margin-bottom: 1em;
-	}
-	&__intro {
+		margin-left: auto;
+		margin-right: auto;
+		blur: 5;
+	
+	&__intro 
 		opacity: .8;
-	}
-	&__site-title {
+	
+	&__site-title 
 		font-size: 1.5em;
-	}
-	&__links {
+	
+	&__links
 		margin-top: -.5em;
-		a {
+		a
 			margin: 0 .5em;
-			color: #FFF01A;
-			&:hover {
-				color: #002F3C;
-			}
-		}
-	}
-}
+		
+	
+
 </style>
